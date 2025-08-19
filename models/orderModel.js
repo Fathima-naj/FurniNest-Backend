5d1c352f4b89d9ca1bfe823887e33c55b2b28be7
Fathima-naj <fathimanajamuneer@gmail.com>
2025-08-19 16:15:44 +0530

    ecommerce backend

---
+++ models/orderModel.js
@@ -0,0 +1,37 @@
+const mongoose=require('mongoose')
+
+const orderSchema=new mongoose.Schema({
+    user:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+    items:[
+        {
+            productId:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},
+            quantity:{type:Number,required:true},
+        },
+    ],
+    date:{type:Date,default:Date.now,required:true},
+    name:{type:String},
+    address:{type:String,required:true},
+    paymentMethod:{type:String,required:true},
+    total:{type:Number,required:true},
+    razorpayOrderId: {
+        type: String,
+      },
+      razorpayPaymentId: {
+        type: String,
+      },
+      status: {
+        type: String,
+        enum: ["placed", "shipped", "delivered", "pending", "cancelled"],
+      },
+      razorpayPaymentStatus: {
+        type: String,
+        enum: ["paid", "failed", "pending", "captured", "refunded"],
+        default: "pending",
+      },
+    },
+    {timestamps:true}
+
+)
+
+const Order=mongoose.model('Order',orderSchema)
+module.exports=Order;
---
+++ routes/orderRoutes.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const authenticate=require('../middlewares/authMiddleware')
+const { addOrder, showOrders, verifyPayment } = require('../controllers/orderController')
+
+const router=express.Router()
+
+router.post('/addToOrder',authenticate,addOrder)
+router.post('/verifypayment',verifyPayment)
+router.get('/showOrder',authenticate,showOrders)
+
+module.exports=router
